c44339a3002372c5881368af369fd4f785dd0039
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-07-10 14:55:30 -0300

    setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+     BrowserModule,
+     IonicModule.forRoot(),
+     AppRoutingModule
+     AngularFireModule.inicializeApp({
+       apiKey: "AIzaSyCPmEZMaLKI_iLp4yYj09AgfBz2hnAdSL8",
+       authDomain: "anilab-c5e5a.firebaseapp.com",
+       databaseURL: "https://anilab-c5e5a.firebaseio.com",
+       projectId: "anilab-c5e5a",
+       storageBucket: "",
+       messagingSenderId: "437504453400",
+       appId: "1:437504453400:web:6423880c3c7744bb"
+     }),
+     AngularFireDatabaseModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
