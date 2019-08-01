7ce2261114362cd7366d5a2c3c9cdb0a0a2927fa
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-08-01 20:34:10 -0300

    implementacao de login e logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'form-anime', loadChildren: './anime/form-anime/form-anime.module#FormAnimePageModule' },
   { path: 'list-anime', loadChildren: './anime/list-anime/list-anime.module#ListAnimePageModule' },
   { path: 'view-anime', loadChildren: './anime/view-anime/view-anime.module#ViewAnimePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from "angularfire2/auth";
+import { Router } from '@angular/router';
+
+import { Usuario } from '../model/usuario';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  usuario: Usuario = new Usuario();
+
+  constructor(private router: Router, private authService: AngularFireAuth) { }
+
+  ngOnInit() {
+  }
+
+  login(): void{
+    this.authService.auth.signInWithEmailAndPassword(this.usuario.email,
+       this.usuario.senha).then((res) => {
+         this.router.navigate(['home']);
+       }).catch( (erro) => console.log(erro) );
+  }
+
+  logout(): void{
+    this.authService.auth.signOut();
+    this.router.navigate(['login']);
+  }
+
+}
