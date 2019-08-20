c7baad2632b76864248f57045c6bbcb46cbedfe5
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-08-20 16:42:36 -0300

    implementacao de validacao do furmulario de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -12,28 +12,44 @@ import { Usuario } from '../model/usuario';
 export class LoginPage implements OnInit {
 
   usuario: Usuario = new Usuario();
+  error: string = "";
 
   constructor(private router: Router, private authService: AngularFireAuth) { }
 
   ngOnInit() {
+
   }
 
-  login(): void{
-    this.authService.auth.signInWithEmailAndPassword(this.usuario.email,
-       this.usuario.senha).then((res) => {
-         this.router.navigate(['home']);
-       }).catch( (erro) => console.log(erro) );
+  login(): void {
+
+    if (this.usuario.email && this.usuario.senha) {
+      this.error = "";
+      
+      this.authService.auth.signInWithEmailAndPassword(this.usuario.email,
+        this.usuario.senha)
+        .then((res) => {
+          this.error="";
+          this.router.navigate(['home']);
+        }).catch((erro) => {
+          this.error="Email ou senha inválidos";
+          console.log(erro);
+        });
+
+    } else {
+      this.error = "Preencha todos os campos!";
+    }
+
   }
 
-  logout(): void{
+  logout(): void {
     this.authService.auth.signOut();
     this.router.navigate(['login']);
   }
 
-  redefinePwd(): void{
+  redefinePwd(): void {
     this.authService.auth.sendPasswordResetEmail(this.usuario.email)
-      .then( (res) => alert('Email enviado!'))
-      .catch( (err) => console.log(err));
+      .then((res) => alert('Email enviado!'))
+      .catch((err) => console.log(err));
   }
 
 }
