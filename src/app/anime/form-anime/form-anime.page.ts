9a1a53248ea909301b4d135a2b8dd2fb08e257c5
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-08-20 17:10:31 -0300

    finalizacao da implementacao das validacoes

--- src/app/anime/form-anime/form-anime.page.ts
+++ src/app/anime/form-anime/form-anime.page.ts
@@ -15,49 +15,63 @@ export class FormAnimePage implements OnInit {
 
   key: string;
   anime: Anime = new Anime();
+  error: string;
 
   constructor(private fire: AngularFireDatabase,
-     private rota: Router,
-     private route: ActivatedRoute,
-     private authService: AngularFireAuth) { }
+    private rota: Router,
+    private route: ActivatedRoute,
+    private authService: AngularFireAuth) { }
 
   ngOnInit() {
-    
-    this.authService.authState.subscribe( (user) => {
-      if(user){
-        if(user.displayName){
+
+    this.authService.authState.subscribe((user) => {
+      if (user) {
+        if (user.displayName) {
           this.anime.autor = user.displayName;
-        }else{
+        } else {
           this.anime.autor = user.email;
         }
         this.anime.user_id = user.uid;
-      }else{
+      } else {
         this.rota.navigate(['login']);
       }
     });
-    
-    this.route.queryParams.subscribe( params => {
-      if(params['key']){
+
+    this.route.queryParams.subscribe(params => {
+      if (params['key']) {
         this.key = params['key'];
-        (this.fire.object('anime/'+this.key)
+        (this.fire.object('anime/' + this.key)
           .valueChanges() as Observable<Anime>)
           .subscribe(value => this.anime = value);
       }
     });
-  
+
   }
 
-  salvar() : void{
-    
-    if(this.key){
-      this.fire.object('anime/'+this.key).update(this.anime);
-    }else{
-      this.fire.list('anime').push(this.anime);
+  salvar(): void {
+
+    if(this.validForm()){
+      if (this.key) {
+        this.fire.object('anime/' + this.key).update(this.anime);
+      } else {
+        this.fire.list('anime').push(this.anime);
+      }
+  
+      this.anime = new Anime();
+      this.rota.navigate(['list-anime']);
     }
+    
+  }
 
-    this.anime = new Anime();
-    this.rota.navigate(['list-anime']);
-
+  validForm(): Boolean {
+    if (this.anime.autor && this.anime.demografia
+      && this.anime.sinopse && this.anime.titulo) {
+      this.error="";
+      return true;
+    } else {
+      this.error="Preencha todos os campos";
+      return false;
+    }
   }
 
 }
